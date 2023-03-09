079f71492bdbf60a4e3be3535c5cf025f85fb894
Gabriel-Robles-Cueva <113194750+Gabriel-Robles-Cueva@users.noreply.github.com>
2023-03-09 21:11:28 +0100

    Update

--- API/.venv/app.py
+++ API/.venv/app.py
@@ -1,11 +1,52 @@
-from flask import Flask
-import mysql.connector
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
 
-app= Flask(__name__)
-conexion1=mysql.connector.connect(host="localhost", user="root", passwd="")
-cursor1=conexion1.cursor()
+app = Flask(__name__)
 
-cursor1.execute("show databases")
-for base in cursor1:
-    print(base)
-conexion1.close() 
+#MySQL Connection
+app.config['MYSQL_HOST']= 'localhost'
+app.config['MYSQL_USER']= 'root'
+app.config['MYSQL_PASSWORD']= ''
+app.config['MYSQL_DB']= 'db_objetos'
+mysql = MySQL(app)
+
+#Setting
+app.secret_key= 'mysecretkey'
+
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+@app.route('/add-object')
+def añadirObjeto():
+    return render_template('añadir.html')
+
+@app.route('/object-added', methods=['POST'])
+def objetoAñadido():
+    if request.method == 'POST':
+        nombre= request.form['nombre']
+        tipo= request.form['tipo']
+        precio= request.form['precio']
+        print(nombre)
+        print(tipo)
+        print(precio)
+        cur= mysql.connection.cursor()
+        cur.execute('INSERT INTO objetos (nombre,tipo,precio) VALUES (%s, %s, %s)', (nombre,tipo,precio))
+        mysql.connection.commit()
+        flash('Objeto añadido correctamente')
+        return redirect(url_for('añadirObjeto'))
+
+@app.route('/get')
+def verObjetos():
+    return 'see_object'
+
+@app.route('/edit')
+def editarObjeto():
+    return 'edit_object'
+
+@app.route('/delete')
+def eliminarObjeto():
+    return 'delete-object'
+
+if __name__=='__main__':
+    app.run(port = 3000, debug= True)
